93e4923345793efd86b60ccc32b4eff8840b3110
LimHM <gusals3407@gmail.com>
2017-03-11 20:50:22 +0900

    Add user_img property @MyUser

--- django_app/member/admin.py
+++ django_app/member/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 from member.models import MyUser
 
 
 class MyUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'user_img')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
 
 
 admin.site.register(MyUser, MyUserAdmin)
--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class MyUser(AbstractUser):
-    pass
+    user_img = models.ImageField(upload_to='user', blank=True)
